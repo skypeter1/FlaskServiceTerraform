41af4abcc6eb8202b30313b25f9e28b684b5e4dc
skypeter1 <skypeter1@hotmail.com>
2020-02-19 14:09:28 +0100

    Add setup config for the service

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -7,7 +7,9 @@ echo added user to docker group
 
 sudo apt-get update -y
 sudo apt-get install git -y
+sudo apt-get install build-essential
 
-# cd /home/ubuntu
-# git clone  
-# sudo make docker-run
+
+cd /home/ubuntu
+git clone  https://github.com/skypeter1/FlaskServiceTerraform.git
+sudo make docker-run
